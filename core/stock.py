88159fe76d8ecda5d2ce3e97b747f4e6dc245d54
Lil Blick <chrispencer@berkeley.edu>
2024-08-28 18:26:52 -0700

    add get stock method

--- core/stock.py
+++ core/stock.py
@@ -4,8 +4,18 @@ class CurrentStock:
     def __init__(self, restaurant_id: str, token: str):
         self.base_url = "https://toast-api-server/stock/v1/inventory"
         self.headers = {
-            "Toast-Restaurant-External-ID": "string",
-            "Authorization": "Bearer <YOUR_TOKEN_HERE>"
+            "Toast-Restaurant-External-ID": restaurant_id,
+            "Authorization": f"Bearer {token}"
         }
 
+    def get_stock(self, status="QUANTITY"):
+        query = {
+            "status": status
+        }
+        response = requests.get(self.base_url, headers=self.headers, params=query)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
     
